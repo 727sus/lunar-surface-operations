ab57679e81a2f2099fd177829bec2457c5ffeef4
gyataro <teoh6g@gmail.com>
2021-10-02 10:38:55 +0800

    Added proptype checking, refactored log card code

--- lunar_frontend/src/utils/util.user.js
+++ lunar_frontend/src/utils/util.user.js
@@ -7,18 +7,18 @@ function djb2(str){
     return hash;
 }
 
+// Check if user is logged in
+function isAuthenticated() {
+    return true;
+}
+
 // Assign each username/id a color
-function hashStringToColor(str) {
+function getUserColor(str) {
     var hash = djb2(str);
     var r = (hash & 0xFF0000) >> 16;
     var g = (hash & 0x00FF00) >> 8;
     var b = hash & 0x0000FF;
     return "#" + ("0" + r.toString(16)).substr(-2) + ("0" + g.toString(16)).substr(-2) + ("0" + b.toString(16)).substr(-2);
-}
-
-// Check if user is logged in
-function isAuthenticated() {
-    return false;
 }
 
 // Returns current logged in username
@@ -27,7 +27,7 @@ function getUsername() {
 }
 
 export {
-    hashStringToColor,
     isAuthenticated,
-    getUsername
+    getUsername,
+    getUserColor
 }
