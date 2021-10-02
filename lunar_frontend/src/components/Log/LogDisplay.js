5183f634d89e9f505d73328ec4a36fb8392a8775
hoolee2014 <74129997+hoolee2014@users.noreply.github.com>
2021-10-02 21:12:06 +0800

    Added Individual Text Differentiation

--- lunar_frontend/src/components/Log/LogDisplay.js
+++ lunar_frontend/src/components/Log/LogDisplay.js
@@ -2,38 +2,27 @@ import React, { useState } from 'react';
 import { Flex, Box, Center, Text } from '@chakra-ui/react';
 
 const LogDisplay = () => {
+  const message = { fromMe: true, text: 'Hello' };
   return (
     <Center>
-      <Flex
-        direction="column"
-        grow="1"
-        color="brand.accent2"
-        bg="brand.secondary"
-      >
+      <Flex direction="column" grow="1" color="brand.accent2" bg="white">
         <Flex grow="1" overflow="auto">
           <Flex direction="column" align="start" justify="end" px="3">
-            <Flex my="1" direction="column">
-              <Box rounded="md" py="1" py="2">
-                Hello
-              </Box>
-              <Box rounded="md" py="1" py="2">
-                <Text align-self="end">Wassup</Text>
-              </Box>
-              <Box rounded="md" py="1" py="2">
-                <Text align-self="end">Wassup</Text>
-              </Box>
-              <Box rounded="md" py="1" py="2">
-                <Text align-self="end">Wassup</Text>
-              </Box>
-              <Box rounded="md" py="1" py="2">
-                <Text align-self="end">Wassup</Text>
-              </Box>
-              <Box rounded="md" py="1" py="2">
-                <Text align-self="end">Wassup</Text>
-              </Box>
-              <Box rounded="md" py="1" py="2">
-                <Text align-self="end">Wassup</Text>
-              </Box>
+            <Flex my="1" direction="column" alignSelf="end" alignItems="end">
+              <div
+                className={`rounded px-2 py-1 ${
+                  message.fromMe ? 'bg-primary text-white' : 'border'
+                }`}
+              >
+                {message.text}
+              </div>
+              <div
+                className={`text-muted small ${
+                  message.fromMe ? 'text-right' : ''
+                }`}
+              >
+                {message.fromMe ? 'You' : message.senderName}
+              </div>
             </Flex>
           </Flex>
         </Flex>
--- lunar_frontend/src/components/Log/LogInput.js
+++ lunar_frontend/src/components/Log/LogInput.js
@@ -1,21 +1,51 @@
 import React from 'react';
-import { Box, Center, Input, Button, Flex } from '@chakra-ui/react';
+import {
+  Box,
+  Center,
+  Input,
+  Button,
+  Flex,
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+  useDisclosure,
+} from '@chakra-ui/react';
 
-class LogInput extends React.Component {
-  render() {
-    return (
-      <Center>
-        <Box w="100%" color="brand.secondary">
-          <Flex>
-            <Input placeholder="Enter log" color="brand.accent2" />
-            <Button type="submit" color="brand.accent2">
-              Send
-            </Button>
-          </Flex>
-        </Box>
-      </Center>
-    );
-  }
-}
+import { ArrowUpIcon, PlusSquareIcon } from '@chakra-ui/icons';
+
+const LogInput = () => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  return (
+    <Box w="100%" color="brand.secondary">
+      <Flex>
+        <Button bg="white" color="brand.accent2" onClick={onOpen}>
+          <ArrowUpIcon />
+        </Button>
+
+        <Modal isOpen={isOpen} onClose={onClose}>
+          <ModalOverlay />
+          <ModalContent>
+            <ModalHeader>Upload image</ModalHeader>
+            <ModalCloseButton />
+            <ModalBody>
+              <Center>
+                <PlusSquareIcon m="200" w={12} h={12} />
+              </Center>
+            </ModalBody>
+          </ModalContent>
+        </Modal>
+
+        <Input placeholder="Enter log" color="brand.accent2" />
+        <Button type="submit" color="brand.accent2">
+          Send
+        </Button>
+      </Flex>
+    </Box>
+  );
+};
 
 export default LogInput;
