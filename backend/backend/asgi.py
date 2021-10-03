e5d59a4c7323f8bc9219478d9ace13435eb7b925
siew24 <maxsieweeshin@gmail.com>
2021-10-03 16:26:06 +0800

    Working build

--- backend/backend/asgi.py
+++ backend/backend/asgi.py
@@ -8,13 +8,15 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
 """
 
 import os
-
-from channels.auth import AuthMiddlewareStack
-from channels.routing import ProtocolTypeRouter, URLRouter
-from django.core.asgi import get_asgi_application
-import api.routing
-
+import django
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
+django.setup()
+
+
+import api.routing
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
+from django.core.asgi import get_asgi_application
 
 application = ProtocolTypeRouter({
     'http': get_asgi_application(),
