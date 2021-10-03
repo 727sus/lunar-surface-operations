71cff133eb679a85cd96ffe5483214733028609b
WKYong <ywk918@gmail.com>
2021-10-03 11:17:13 +0800

    Added POST & DELETE views for log/<str:log_id> endpoint
    1. Added POST request views for log/<str:log_id> endpoint
    	1. Acts to permanently save log
    2. Added DELETE request views for log/<str:log_id> endpoint
    	1. Acts to delete log
    3. Added Tests for POST and DELETE

--- backend/api/tests.py
+++ backend/api/tests.py
@@ -3,11 +3,13 @@ from rest_framework.test import APITestCase
 from django.contrib.auth.models import User
 
 from api.serializers import LogSerializer
+from backend.settings import MEDIA_ROOT
 from .models import Log, File
-from rest_framework import status
+from rest_framework import response, status
 from django.urls import reverse
 from django.forms.models import model_to_dict
-from tempfile import mkstemp
+from tempfile import gettempdir
+from django.test import override_settings
 
 
 # Create your tests here.
@@ -158,15 +160,158 @@ class LogModelTest(APITestCase):
         self.assertNotEqual(
             log.log_text, "This is my current logging progress as non author")
 
+    def test_post_log_with_auth(self):
+
+        update_log = reverse("log_view", kwargs={"log_id": "1"})
+        data = {"username": "test-user",
+                "email": "test@mail.com",
+                "password": "test-password",
+                }
+
+        self.client.post(self.login, data)
+
+        data = {"log_text": "This is my final log update",
+                "other_fields": {"Flight seat": 55, "Duration of current Mission": 215}
+                }
+
+        response = self.client.post(update_log, data, format="json")
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data["log_text"],
+                         "This is my final log update")
+        self.assertDictEqual(response.data["other_fields"],
+                             {"Flight seat": 55,
+                             "Duration of current Mission": 215})
+        self.assertTrue(response.data["perm_save"])
+
+    def test_operations_after_post(self):
+
+        log_endpoint = reverse("log_view", kwargs={"log_id": "1"})
+        data = {"username": "test-user",
+                "email": "test@mail.com",
+                "password": "test-password",
+                }
+
+        self.client.post(self.login, data)
+
+        data = {"log_text": "This is my final log update",
+                "other_fields": {"Flight seat": 55, "Duration of current Mission": 215}
+                }
+        # POST request to permanently save log
+        self.client.post(log_endpoint, data, format="json")
+
+        data = {"log_text": "This is my log update after updating, which should fail",
+                "other_fields": {"Flight seat": 10, "Duration of current Mission": 55}
+                }
+
+        # POST request attempt to overwrite
+        response = self.client.post(log_endpoint, data, format="json")
+
+        self.assertTrue(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+        # PUT request
+        response = self.client.patch(log_endpoint, data, format="json")
+
+        self.assertTrue(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+        # DELETE request
+        response = self.client.delete(log_endpoint)
+
+        self.assertTrue(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+        response = self.client.get(log_endpoint)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data["log_text"],
+                         "This is my final log update")
+
+    def test_post_log_without_auth(self):
+        log_endpoint = reverse("log_view", kwargs={"log_id": "1"})
+        data = {"log_text": "This is my final log update",
+                "other_fields": {"Flight seat": 55, "Duration of current Mission": 215}
+                }
+
+        response = self.client.post(log_endpoint, data, format="json")
+
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+    def test_post_log_with_different_auth(self):
+        User.objects.create_user(
+            username="test-user-2",
+            email="test-2@mail.com",
+            password="test-password-2",
+        )
+
+        data = {"username": "test-user-2",
+                "email": "test-2@mail.com",
+                "password": "test-password-2",
+                }
+
+        self.client.post(self.login, data=data)
+
+        log_endpoint = reverse("log_view", kwargs={"log_id": "1"})
+
+        data = {"log_text": "This is my log update after updating, which should fail",
+                "other_fields": {"Flight seat": 10, "Duration of current Mission": 55}
+                }
+
+        response = self.client.post(log_endpoint, data, format="json")
+
+        self.assertTrue(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+        response = self.client.get(log_endpoint)
+
+        self.assertNotEqual(response.data["log_text"],
+                            "This is my log update after updating, which should fail")
+
     def test_delete_log_with_auth(self):
-        return
+        log_endpoint = reverse("log_view", kwargs={"log_id": "1"})
+        data = {"username": "test-user",
+                "email": "test@mail.com",
+                "password": "test-password",
+                }
+
+        self.client.post(self.login, data=data)
+
+        response = self.client.delete(log_endpoint)
+
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+
+        response = self.client.get(log_endpoint)
+
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
 
     def test_delete_log_without_auth(self):
-        return
+        log_endpoint = reverse("log_view", kwargs={"log_id": "1"})
+
+        response = self.client.delete(log_endpoint)
+
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
 
     def test_delete_log_with_different_auth(self):
-        return
+        User.objects.create_user(
+            username="test-user-2",
+            email="test-2@mail.com",
+            password="test-password-2",
+        )
 
+        data = {"username": "test-user-2",
+                "email": "test-2@mail.com",
+                "password": "test-password-2",
+                }
+
+        self.client.post(self.login, data=data)
+
+        log_endpoint = reverse("log_view", kwargs={"log_id": "1"})
+
+        response = self.client.delete(log_endpoint)
+
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+        response = self.client.get(log_endpoint)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    @override_settings(MEDIA_ROOT=gettempdir())
     def test_upload_file(self):
 
         file = SimpleUploadedFile(
